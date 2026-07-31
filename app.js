2a0c077d7fda623f80c07d32c22b3aa0df1c7a22
camila <Camila@gmail.com>
2026-07-31 13:49:55 -0500

    Agregado el port para que me muestre la consola

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (__, res) => {
 });
 
 app.listen(port, () => {
-    console.log( `Servidor en funcionamiento en el puerto: ` );
+    console.log( `Servidor en funcionamiento en el puerto: ${port}` );
 });
