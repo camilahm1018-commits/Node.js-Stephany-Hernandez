96144f0c6a7658c70bd1cef128bffdab686f8b6b
camila <Camila@gmail.com>
2026-08-28 16:52:22 -0500

    envio de informacion de json y form de aprendices

--- Documents/Stephany-Hernandez/intro-express-js-Stephany-Hernandez/app.js
+++ Documents/Stephany-Hernandez/intro-express-js-Stephany-Hernandez/app.js
@@ -1,11 +1,80 @@
 const express = require('express');
 const app= express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PUERTO || 3000;
+//middleware para parsear datos del body
+app.use(express.json()) 
+app.use (express.urlencoded({extended:true}))
 
-app.get('/', (__, res) => {
+app.get('/', (req, res) => {
     res.send('Aprendicez ficha 3407186');
 });
 
+
+//endpoint para listar aprendices
+app.get('/api/aprendices', (req , res) => {
+    res.status(200).json ({
+        "mensaje":"Lista de aprendices"
+         
+    })
+})
+//endpoint para Listar un aprendiz
+
+app.get('/api/aprendices/:id',(req, res) =>{
+    res.status(200).json ({
+        "mensaje":"Listar un aprendiz"
+         
+    })
+})
+
+//endpoint para crear aprendices
+
+app.post('/api/aprendices',(req, res) =>{
+    res.status(201).json ({
+        "mensaje":"Crear aprendices"
+         
+    })
+})
+
+//endpoint para editar aprendices
+
+app.put('/api/aprendices/:id',(req, res) =>{
+    res.status(200).json ({
+        "mensaje":"Editar aprendices"
+         
+    })
+})
+
+//endpoint para Eliminar aprendices
+
+app.delete('/api/aprendices/:id',(req, res) =>{
+    res.status(200).json ({
+        "mensaje":"Eliminar aprendices"
+         
+    })
+})
+
+//
+app.post("/rutaJson", (req, res)=>{
+    const todosDatos =req.body
+    const edad =req.body.Edad
+    if (edad >= 18) {
+        res.json({"mensaje":"Es mayor de edad"})
+    }else {
+        res.json({"mensaje":"Es menor"})
+    }
+    res.json({datosJson: todosDatos})
+})
+
+app.post("/rutaFormularios", (req, res)=>{
+    const todosDatos =req.body
+    const programa = req.body.programa
+    
+    res.json({Todosdatos: todosDatos, Mi_Programa: programa})
+})
+
+
 app.listen(port, () => {
-    console.log( `Servidor en funcionamiento en el puerto: ` );
+    console.log( `Servidor: http://localhost:${port}` );
 });
+
